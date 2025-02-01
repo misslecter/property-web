a548e328224cc653d4ed0f83280fc1446f04f559
tomaskourim <kourim@outlook.com>
2025-02-01 17:03:05 +0100

    remove en desc the old way

--- src/components/PropertyDescription.vue
+++ src/components/PropertyDescription.vue
@@ -11,20 +11,8 @@ if (!info) {
 
 <template>
   <div class="py-8">
-    <a
-      v-if="info.descriptionEn?.length"
-      href="#english"
-      class="italic text-primary opacity-80 transition-opacity hover:opacity-90"
-      >Go to the English description</a
-    >
     <p v-for="(part, index) of info.description" :key="index" class="my-4 leading-loose">
       {{ part }}
     </p>
-    <div v-if="info.descriptionEn?.length" id="english" class="pt-4">
-      <hr class="opacity-40" />
-      <p v-for="(part, index) of info.descriptionEn" :key="index" class="mb-4 mt-8 leading-loose">
-        {{ part }}
-      </p>
-    </div>
   </div>
 </template>
