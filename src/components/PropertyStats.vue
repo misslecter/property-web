c010c0df3f76f92ffb54595bd04f0900a48ffc39
tomaskourim <kourim@outlook.com>
2025-02-02 11:29:46 +0100

    stats english

--- src/components/PropertyStats.vue
+++ src/components/PropertyStats.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { inject } from "vue";
-import { GLOBAL_INFO_KEY } from "@/model/constants.ts";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
 
-const info = inject(GLOBAL_INFO_KEY);
+// Access Vue I18n
+const { tm } = useI18n();
 
-if (!info) {
-  throw new Error("Info not provided");
-}
+// Compute stats dynamically from i18n
+const stats = computed(() => tm('stats'));
 </script>
 
 <template>
   <div class="-mx-4 grid grid-cols-2 gap-px sm:-mx-6 lg:grid-cols-4">
-    <div v-for="stat in info.stats" :key="stat.name" class="px-4 py-6 sm:px-6 lg:px-8">
+    <div v-for="(stat, index) in stats" :key="index" class="px-4 py-6 sm:px-6 lg:px-8">
       <p class="mb-2 flex items-baseline gap-x-2">
         <span class="text-4xl font-semibold tracking-tight">{{ stat.value }}</span>
         <span v-if="stat.unit" class="text-sm text-gray-400">{{ stat.unit }}</span>
