40b59c7df878b0f37cc0975033c0f94712caf2cc
tomaskourim <kourim@outlook.com>
2025-02-02 11:33:51 +0100

    desc english

--- src/components/PropertyDescription.vue
+++ src/components/PropertyDescription.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { inject } from "vue";
-import { GLOBAL_INFO_KEY } from "@/model/constants.ts";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
 
-const info = inject(GLOBAL_INFO_KEY);
+// Access Vue I18n
+const { tm } = useI18n();
 
-if (!info) {
-  throw new Error("Info not provided");
-}
+// Compute the description dynamically from i18n
+const description = computed(() => tm('description'));
 </script>
 
 <template>
   <div class="py-8">
-    <p v-for="(part, index) of info.description" :key="index" class="my-4 leading-loose">
+    <p v-for="(part, index) in description" :key="index" class="my-4 leading-loose">
       {{ part }}
     </p>
   </div>
