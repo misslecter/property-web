ce68ebf31099c65a21c969d13d83ebdd91e9bcc0
tomaskourim <kourim@outlook.com>
2025-02-02 11:23:58 +0100

    badges english

--- src/components/layout/AppBadges.vue
+++ src/components/layout/AppBadges.vue
@@ -1,35 +1,37 @@
 <script setup lang="ts">
-import { inject } from "vue";
-import { GLOBAL_INFO_KEY } from "@/model/constants.ts";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
 import { ArrowDownTrayIcon } from "@heroicons/vue/24/outline";
 import AppLinkWithIcon from "@/components/layout/AppLinkWithIcon.vue";
 
-const appBasePath = import.meta.env.VITE_BASE_PATH;
-const info = inject(GLOBAL_INFO_KEY);
+// Access Vue I18n
+const { t, tm } = useI18n();
 
-if (!info) {
-  throw new Error("Info not provided");
-}
+// Compute badges dynamically from i18n
+const badges = computed(() => tm('badges'));
+
+const appBasePath = import.meta.env.VITE_BASE_PATH;
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center gap-12 lg:flex-row">
     <div
-      v-for="(badge, index) in info.badges"
+      v-for="(badge, index) in badges"
       :key="index"
-      :class="{ 'animate-fadeInDelayed': index === 0, 'animate-fadeInDelayed2': index === 1 }"
+      :class="{ 'animate-fadeInDelayed': Number(index) === 0, 'animate-fadeInDelayed2': Number(index) === 1 }"
       class="flex flex-1 items-center justify-center gap-4 opacity-0 md:gap-8"
     >
       <img src="../../assets/img/leaf-left.png" alt="Leaf left" class="w-[40px]" />
+
       <div class="flex flex-1 flex-col items-center gap-2">
-        <div
-          class="whitespace-nowrap text-center font-bold italic md:text-lg"
-          v-html="'&quot;' + badge.title + '&quot;'"
-        />
+        <!-- Title translated using Vue I18n -->
+        <div class="whitespace-nowrap text-center font-bold italic md:text-lg" v-html="'&quot;' + t(`badges.${index}.title`) + '&quot;'"/>
+
+        <!-- Download link if available -->
         <AppLinkWithIcon
           v-if="badge.download"
-          :title="badge.download.name"
-          :to="`${appBasePath}/${info.name}/${badge.download.basePath}/${badge.download.filename}`"
+          :title="t(`badges.${index}.download.name`)"
+          :to="`${appBasePath}/${t('name')}/${badge.download.basePath}/${badge.download.filename}`"
           :icon="ArrowDownTrayIcon"
           :download="badge.download.filename"
         />
